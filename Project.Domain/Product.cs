899fbaa53cac353d2d033294d8a30d58f2383d56
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2024-05-27 15:03:04 +0300

    Creating Product

---
+++ Project.Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
